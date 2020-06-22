53711e26e7470fa9e29c38b8121f8204c4e660e8
Jason Feuerstein <jwfeuers@usc.edu>
2020-06-22 16:46:38 -0700

    Version 1

--- test-reversi.cpp
+++ test-reversi.cpp
@@ -5,27 +5,20 @@
 
 using namespace std;
 
-
-/**
- * main - Entry point for Reversi
- *  should create a Reversi object using the dimension
- *  provided as the 1st command line argument, or, if
- *  no argument is provided, default to 4.
- *  Once created, call Reversi::play() on the Reversi
- *  object and then return 0;
- */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     size_t size = 4;
     if (argc >= 2)
     {
         size = atoi(argv[1]);
-        if( ((size % 2) == 1) ||
-                ((size < 4) || (size > 26)) ) {
+        if (((size % 2) == 1) ||
+            ((size < 4) || (size > 26)))
+        {
             cout << "Invalid size" << endl;
             return 1;
         }
-        else {
+        else
+        {
             size = atoi(argv[1]);
         }
     }
@@ -33,4 +26,3 @@ int main(int argc, char* argv[])
     game.play();
     return 0;
 }
-
